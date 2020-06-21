7c012a5c251886f83660cb08efd79a6734487b92
LavaPower <lavapower84@gmail.com>
2020-06-21 02:36:39 +0200

    Basic WebView

--- src/fr/lavapower/heartbrowser/HeartBrowser.java
+++ src/fr/lavapower/heartbrowser/HeartBrowser.java
@@ -1,8 +1,30 @@
 package fr.lavapower.heartbrowser;
 
-public class HeartBrowser
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class HeartBrowser extends Application
 {
+    private static Stage stage;
+
+    @Override
+    public void start(Stage stage) throws Exception
+    {
+        HeartBrowser.stage = stage;
+
+        Parent parent = FXMLLoader.load(getClass().getResource("browser.fxml"));
+        Scene scene = new Scene(parent);
+
+        stage.setMaximized(true);
+        stage.setTitle("HeartBrowser");
+        stage.setScene(scene);
+        stage.show();
+    }
+
     public static void main(String[] args) {
-        System.out.println("launch");
+        launch(args);
     }
 }
