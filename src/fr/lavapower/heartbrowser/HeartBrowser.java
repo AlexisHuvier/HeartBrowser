56d567759b7efa9bda1c2132c34361b0574e724f
LavaPower <lavapower84@gmail.com>
2020-07-28 03:38:33 +0200

    Make some variables static for future use.

--- src/fr/lavapower/heartbrowser/HeartBrowser.java
+++ src/fr/lavapower/heartbrowser/HeartBrowser.java
@@ -7,10 +7,15 @@ import javafx.stage.Stage;
 
 public class HeartBrowser extends Application
 {
+    private static Grid grid;
+    public static Stage stage;
+
     @Override
     public void start(Stage stage)
     {
-        Grid grid = new Grid();
+        HeartBrowser.stage = stage;
+
+        grid = new Grid();
         Scene scene = new Scene(grid, 780, 640);
 
         stage.setMaximized(true);
